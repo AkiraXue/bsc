d9fd0238753c4afa4b2d25a168eb1124bf474d14
akiraxue <xuelei@corp-ci.com>
2021-05-29 15:31:13 +0800

    feat: update upload code

--- etc/env.php
+++ etc/env.php
@@ -31,9 +31,19 @@ define('AES_KEY', '');
 
 
 /** upload config */
-define('ARCHIVE_PATH',  'resource/upload/');
+define('ARCHIVE_PATH',  'upload/');
 define('ALLOW_RESOURCE_TYPE', 'gif|jpg|png|jpeg|mp4');
-define('MAX_BUFFER_LIMIT', '2048');
+define('MAX_BUFFER_LIMIT', '512000');
 
-//define('CDN_HOST', 'https://cdn.akiraxue.com');
-define('CDN_HOST', 'https://cdn.sixtyden.com');
+// define('CDN_HOST', 'https://cdn.akiraxue.com');
+define('CDN_HOST', 'https://cdn.bsc.com/');
+
+/** ali oss config */
+define('ALI_OSS_ACCESS_KEY_ID', 'LTAI5tJo9ujyKrPktDmB194r');
+define('ALI_OSS_ACCESS_KEY_SECRET', 'yeNSgjoKbusWYSmTfmV1s4EZomHpEc');
+define('ALI_OSS_ENDPOINT', 'https://oss-cn-shanghai.aliyuncs.com/');
+define('ALI_OSS_BUCKET', 'bageng-bsc-namespace2021');
+define('ALI_OSS_DIR', '/bsc');
+define('ALI_OSS_ROLE', 'acs:ram::1472580472326892:role/aliyunserviceroleforsddp');
+
+
